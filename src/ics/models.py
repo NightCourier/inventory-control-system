0dc2de8edc5a80dc8c754eb73274db0791b5e85c
NightCourier <dan8dmit@gmail.com>
2021-04-18 16:07:00 +0300

    init

---
+++ src/ics/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Storage(models.Model):
+    city = models.CharField(max_length=255, verbose_name="Город")
+    group = models.CharField(max_length=255, verbose_name="Группа")
+
+    def __str__(self):
+        return self.city
+
+    class Meta:
+        verbose_name = "Склад"
+        verbose_name_plural = "Склады"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Категория")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория товара"
+        verbose_name_plural = "Категории товара"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Наименование")
+    bar_code = models.BigIntegerField(verbose_name="Штрих-код")
+    vendor_code = models.CharField(max_length=255, verbose_name="Артикул")
+    amount = models.IntegerField(verbose_name="Количество")
+    price = models.FloatField(verbose_name="Цена")
+    description = models.CharField(max_length=255, verbose_name="Описание")
+    details = models.CharField(max_length=255, verbose_name="Характеристика товара")
+    storage = models.ManyToManyField(Storage, verbose_name="Склад")
+    product_type = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
