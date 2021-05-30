87ff7809a4d02a604d4a7c7bcdd55f26aa6d9035
NightCourier <dan8dmit@gmail.com>
2021-05-30 20:47:21 +0300

    ui refactoring

--- src/config/settings.py
+++ src/config/settings.py
@@ -44,8 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third-party apps
     'crispy_forms',
-
+    # custom apps
+    'accounts',
     'ics',
 ]
 
@@ -117,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
